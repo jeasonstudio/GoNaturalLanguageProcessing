b87491da23ce8fd356e6c05d64aa811453721b2e
赵吉彤 <zjtong3576@sina.com>
2017-02-24 14:42:15 +0800

    shit

--- SQL/main_table.sql
+++ SQL/main_table.sql
@@ -1,7 +1,7 @@
 -- 创建数据库
 CREATE DATABASE naturl_language_process;
 
--- 创建单字表
+-- 创建单字属性表
 CREATE TABLE naturl_language_process.main_single_word
 (
     word_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -10,7 +10,7 @@ CREATE TABLE naturl_language_process.main_single_word
 );
 CREATE UNIQUE INDEX main_single_word_word_id_uindex ON naturl_language_process.main_single_word (word_id);
 
--- 创建词汇表
+-- 创建词汇属性表
 CREATE TABLE naturl_language_process.main_single_terms
 (
     terms_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
--- main.go
+++ main.go
@@ -1,36 +1,67 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/jinzhu/gorm"
 )
 
 // HOST USER PASSWORD DBNAME
 const (
-	HOST     = "127.0.0.1"
-	PORT     = "3306"
-	USER     = "root"
-	PASSWORD = ""
-	DBNAME   = "naturl_language_process"
+	HOST          = "127.0.0.1"
+	PORT          = "3306"
+	USER          = "root"
+	PASSWORD      = ""
+	DBNAME        = "naturl_language_process"
+	LANGUAGECLOUD = "f9Z9q9U6uUbDNfmEkEjHKAFbMhBBcftx0N5y7W6f"
+	URL           = "http://api.ltp-cloud.com/analysis/"
 )
 
-var db *sql.DB
+// var db *sql.DB
 
-func init() {
-	db, _ = sql.Open("mysql", USER+":@tcp("+HOST+":"+PORT+")/"+DBNAME+"?charset=utf8")
-	db.SetMaxOpenConns(20)
-	db.SetMaxIdleConns(10)
-	db.Ping()
-}
+// func init() {
+// 	db, _ = sql.Open("mysql", USER+"@tcp("+HOST+":"+PORT+")/"+DBNAME+"?charset=utf8")
+// 	db.SetMaxOpenConns(2000)
+// 	db.SetMaxIdleConns(1000)
+// 	db.Ping()
+// }
 
 func main() {
-	fmt.Println(HOST)
-	rows, _ := db.Query("SELECT * FROM main_single_word WHERE word_id = '4'")
-	defer rows.Close()
+	db, _ := gorm.Open("mysql", USER+"@tcp("+HOST+":"+PORT+")/"+DBNAME+"?charset=utf8")
+	defer db.Close()
 
-	for rows.Next() {
-		var word_id, word_name, word_pinyin string
-		rows.Scan(&word_id, &word_name, &word_pinyin)
-		fmt.Println("word_id:", word_id, "word_name:", word_name, "word_pinyin:", word_pinyin)
+	var user string
+
+	x := db.Where("word_id = ?", "4").Find(&user)
+
+	fmt.Println(x, user)
+
+	getCiXing("你好")
+}
+
+func getCiXing(text string) string {
+	resp, err := http.Get(URL + "?api_key=" + LANGUAGECLOUD + "&text=" + text + "&pattern=pos&format=plain")
+	checkErr(err)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	checkErr(err)
+
+	resArr := strings.Split(string(body), " ")
+	for i := range resArr {
+		thisName := strings.Split(resArr[i], "_")
+		resArr[i] = thisName[1]
+	}
+	whatYouWant := strings.Join(resArr, "/")
+	fmt.Println("Normal Text is", string(body), "CiXing is", whatYouWant, "Success!!")
+	return whatYouWant
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
 	}
 }
